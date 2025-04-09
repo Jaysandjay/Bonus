a08f960257f69931104357eb3a721258bf7d81e0
Jasmine <jayjadesanders@gmail.com>
2025-04-09 18:35:53 -0400

    Made grid dynamic

--- src/order.js
+++ src/order.js
@@ -94,16 +94,26 @@ function storeOrder(){
     localStorage.setItem("currentOrder", JSON.stringify(order))
 }
 
+function createGridData(){
+    let order = JSON.parse(localStorage.getItem("currentOrder"))
+    let total = ["", "Total", `$${order.total}.00`]
+    delete order.total
+    let gridData = []
+    for(const pizza in order){
+        let column = []
+        column.push(pizza)
+        column.push(order[pizza].quantity)
+        column.push(`$${order[pizza].price}.00`)
+        gridData.push(column)
+    }
+    gridData.push(total)
+    return gridData
+}
+
 function updateOrderDisplay(){
     new Grid({
-        columns: ["Name", "Email", "Phone Number"],
-        data: [
-          ["John", "john@example.com", "(353) 01 222 3333"],
-          ["Mark", "mark@gmail.com", "(01) 22 888 4444"],
-          ["Eoin", "eoin@gmail.com", "0097 22 654 00033"],
-          ["Sarah", "sarahcdd@gmail.com", "+322 876 1233"],
-          ["Afshin", "afshin@mail.com", "(353) 22 87 8356"]
-        ]
+        columns: ["Pizza", "Quantity", "Price"],
+        data: createGridData()
       }).render(document.getElementById("order-details"));
 
 }
